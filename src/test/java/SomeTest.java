9e83f83a2742e28a0e7914bab41f488deb278ee5
Piotr Stefaniak <pstefaniak@atlassian.com>
2013-02-28 17:09:43 +0100

    Update SomeTest.java

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -2,109 +2,22 @@ import junit.framework.TestCase;
 
 public class SomeTest extends TestCase
 {
-    public void testOne() throws Exception
+    public void testAaaa() throws Exception
     {
         new ThisShouldCompile();
-//      fail("one"); 
+        fail("Aaaaa"); 
     }
 
-    public void testTwo() throws Exception
+    public void testPassing() throws Exception
     {
       new ThisShouldCompile();
 //      fail("two"); 
     }
 
-    public void testThree() throws Exception
+    public void testNotPassing() throws Exception
     {
         new ThisShouldCompile();
-        fail("three"); 
+        fail("notPassing"); 
     }
-
-    public void testFour() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("four"); 
-    }
-
-    public void testFive() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("five"); 
-    }
-
-    public void testSix() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six"); 
-    }
-
-    public void testSix1() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six1"); 
-    }
-
-    public void testSix2() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six2"); 
-    }
-
-    public void testSix3() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six3"); 
-    }
-    
-    public void testSix5() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six5"); 
-    }
-    public void testSix4() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six4"); 
-    }
-    public void testSix6() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six6"); 
-    }
-    public void testSix7() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six7"); 
-    }
-    public void testSix8() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six8"); 
-    }
-
-    public void testSix9() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six9"); 
-    }
-
-    public void testSix99() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six99"); 
-    }
-
-    public void testSix899() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six899"); 
-    }
-
-    public void testSix998() throws Exception
-    {
-        new ThisShouldCompile();
-        fail("six998"); 
-    }
-
 
 }
